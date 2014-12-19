727e0db192426a2a5ab9da57fb4a268f87a1c034
PramukJay <iosdeveloper@SachirasiMac.local>
2014-12-19 14:54:40 +0530

    changes

--- www/js/services.js
+++ www/js/services.js
@@ -75,6 +75,7 @@ angular.module('starter.services', ['ngResource'])
 					method: 'GET',
 					params: {username: '@username', pw: '@password'},
 					isArray: false
+					//headers:{'Access-Control-Allow-Origin':'*'}
 				}
 			});
 		}
